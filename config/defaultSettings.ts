322fcccffa06a3f66686adb824124883694122fc
zhanhui1999 <zhanhui.zju@gmail.com>
2023-10-20 19:48:37 +0800

    first commit

---
+++ config/defaultSettings.ts
@@ -0,0 +1,21 @@
+import { Settings as LayoutSettings } from '@ant-design/pro-layout';
+
+const Settings: LayoutSettings & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: '移动应用众包无障碍检测',
+  pwa: false,
+  logo: '//public/acc.svg',
+  iconfontUrl: '',
+};
+
+export default Settings;
